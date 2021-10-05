1907e153803206020c59b9b05fe717db75e2cfd2
Oscar Eduardo Mendoza Alcantara <91708243+OscarEduardoXXOSOXX@users.noreply.github.com>
2021-10-05 14:19:31 -0500

    Add files via upload
    Hola mundo en java

---
+++ Hola.java
@@ -0,0 +1,20 @@
+public class Hola{
+
+	/*
+	 * Aplicación java
+	 * Title	:Hola Mundo en Java
+	 * Description	:Hola Mundo en Java desde UBUNTU
+	 * Author	:Oscar Eduardo Mendoza Alcantara
+	 * Date		:20211004
+	 * Version	:1
+	 * Usage	:c:\>javac /path/Hola.java
+	 * 
+	 * Hola mundo en java*/
+
+	public static void main(String []args){
+	
+		System.out.println("Hola Mundo");
+	
+	}
+
+}
